b3027be642b62997d1b9d909ee23aa47b4142928
Raj kishore Behera <115730942+Bablu08@users.noreply.github.com>
2024-09-16 21:29:46 +0530

    Add files via upload

---
+++ CoreJava/Literals.java
@@ -0,0 +1,19 @@
+package CoreJava;
+
+public class Literals {
+    public static void main(String[] args) {
+
+        byte age = 36;
+        char ch = 'A';
+        float f= 1f;
+        long longNum = 93847539485L;
+        double doubleValue = 4362d;
+        String str = "Raj";
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f);
+        System.out.println(longNum);
+        System.out.println(doubleValue);
+        System.out.println(str);
+    }
+}
