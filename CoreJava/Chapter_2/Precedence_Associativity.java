870c4b3731f3c35f9b9e66128516b6d087688d99
Raj kishore Behera <115730942+Bablu08@users.noreply.github.com>
2024-09-17 18:30:40 +0530

    Add files via upload

---
+++ CoreJava/Chapter_2/Precedence_Associativity.java
@@ -0,0 +1,35 @@
+package CoreJava.Chapter_2;
+
+public class Precedence_Associativity {
+    public static void main(String[] args) {
+
+        int a = 6*5-34/2;
+        /*
+        * = 30 - 34/2
+        * = 30 - 17
+        * = 13
+        * */
+
+        int b = 60/5-34*2;
+        /*
+         * = 12 - 34*2
+         * = 12 - 68
+         * = -54
+         * */
+
+        System.out.println(a);
+        System.out.println(b);
+
+//        Practices
+        int x = 6;
+        int y = 1;
+        int z = x * y/2;
+        /*
+        * Same priority follow Associativity, It means It goes Left - Right
+        * = 6*1 / 2
+        * = 6 / 2
+        * = 3
+        * */
+        System.out.println(z);
+    }
+}
