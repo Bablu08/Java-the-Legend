2be17f536f52b73f3ab9932f57334aa157768524
Raj kishore Behera <115730942+Bablu08@users.noreply.github.com>
2024-09-20 21:31:07 +0530

    Methods.java

--- CoreJava/Chapter_7/Methods.java
+++ CoreJava/Chapter_7/Methods.java
@@ -1,22 +1,44 @@
 package CoreJava.Chapter_7;
 
-public class Method_Overloading {
+public class Methods {
 
-    static void overLoad(){
-        System.out.println("Hii Raj");
+    static void arr(int [] marks){
+        marks[0] = 100;
     }
-    static void overLoad(int a){
-        System.out.println("Hii Raj, your first no: "+a);
+    static void tellJoke(){
+        System.out.println("What does a storm cloud wear under his raincoat? Thunder-wear.");
     }
-    static void overLoad(int a, int b){
-        System.out.println("Hii, Raj your first no:  "+a+ " Your second no: "+b);
+
+    static int sum(int x, int y){
+        int z;
+        if(x > y){
+            z = x+y;
+        }
+        else {
+            z = (x + y) * 5;
+        }
+        return z;
     }
 
     public static void main(String[] args) {
 
-        overLoad();
-        overLoad(59);
-        overLoad(95, 98);
+        int a = 5;
+        int b = 10;
+        int c;
+        c = sum(a,b);
+
+//        Method invocation using Object Creation
+        /*
+        * Methods obj = new Methods();
+        * c = obj.sum(a,b);
+        */
+        System.out.println(c);
+
+        tellJoke();
+
+        int [] mark = {10, 20, 30, 40, 50};
+        arr(mark);
+        System.out.println(mark[0]);
 
     }
 }
