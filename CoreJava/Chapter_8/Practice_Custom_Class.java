2c73c9c16add7ad482f6f850b96e79ddfe34c34c
Raj kishore Behera <115730942+Bablu08@users.noreply.github.com>
2024-09-23 19:33:37 +0530

    Practice_Custom_Class.java

--- CoreJava/Chapter_8/Practice_Custom_Class.java
+++ CoreJava/Chapter_8/Practice_Custom_Class.java
@@ -1,5 +1,7 @@
 package CoreJava.Chapter_8;
 
+import java.util.Scanner;
+
 class Employee1{
     int salary;
     String name;
@@ -27,6 +29,27 @@ class CellPhone{
     }
 }
 
+class Square{
+    int side;
+    public int area(){
+        return side * side;
+    }
+    public int perimeter(){
+        return 4 * side;
+    }
+}
+
+class Rectangle{
+    int width;
+    int height;
+    public int area(){
+        return width * height;
+    }
+    public int perimeter(){
+        return 2 * (width + height);
+    }
+}
+
 public class Practice_Custom_Class {
     public static void main(String[] args) {
 
@@ -34,6 +57,7 @@ public class Practice_Custom_Class {
         Employee1 raj = new Employee1();
         raj.setName("Rajkishore Behera");
         System.out.println(raj.getName());
+        
         raj.salary = 25000;
         System.out.println(raj.getSalary());
 
@@ -42,5 +66,29 @@ public class Practice_Custom_Class {
         phone.ring();
         phone.vibrat();
         phone.callFriend();
+
+//        Problem 3
+        System.out.println("Calculating area and perimeter of the square");
+        Square sq = new Square();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter Number: ");
+        sq.side = sc.nextInt();
+        
+        System.out.println(sq.area());
+        System.out.println(sq.perimeter());
+
+//        Problem 4
+        System.out.println("Calculating area and perimeter of the rectangle");
+        Rectangle rectangle = new Rectangle();
+        
+        System.out.print("Enter height: ");
+        rectangle.height = sc.nextInt();
+        
+        System.out.print("Enter width: ");
+        rectangle.width = sc.nextInt();
+        
+        System.out.println("Area of the rectangle is: " + rectangle.area());
+        System.out.println("Parameter of the rectangle is: " + rectangle.perimeter());
     }
 }
